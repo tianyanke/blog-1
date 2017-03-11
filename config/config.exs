adbae5e9199a8c7d8833f5cddc22f129fe31aff2
bingjie.huang <bingjie.huang@shuyun.com>
2017-03-12 01:30:46 +0800

    init

--- config/config.exs
+++ config/config.exs
@@ -23,7 +23,7 @@ config :logger, :console,
   metadata: [:request_id]
 
 # config slack
-config :slack, api_token: "xoxb-151844922432-BVPX9hameXAGXAoYOUafM1vM"
+config :slack, api_token: "xoxb-151844922432-TBYPySmjVp1qpdyKgFXLS04x"
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
