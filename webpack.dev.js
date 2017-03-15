1e4622e14da36928436ca8202808f8179e9da811
bingjie.huang <bingjie.huang@shuyun.com>
2017-03-15 16:26:38 +0800

    js standard format

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,5 +1,5 @@
-var webpack = require('webpack');
-var path = require('path');
+var webpack = require('webpack')
+var path = require('path')
 
 module.exports = {
   devtool: 'cheap-module-eval-source-map',
@@ -9,29 +9,29 @@ module.exports = {
       'webpack/hot/only-dev-server',
       './web/static/app/app.ts'
     ],
-    vendor: ["react", 'react-dom']
+    vendor: ['react', 'react-dom']
   },
   output: {
     path: path.join(__dirname, './priv/static'),
-    publicPath: "http://localhost:4001/",
-    filename: 'app.js',
+    publicPath: 'http://localhost:4001/',
+    filename: 'app.js'
   },
   module: {
     rules: [
-      { test: /\.png$/, loader: "url-loader?limit=20480&name=images/[hash].[ext]" },
-      { test: /\.jpg|\.svg$/, loader: "file-loader?name=images/[hash].[ext]" },
-      { test: /\.tsx?$/, loaders: ["react-hot-loader/webpack", 'ts-loader'] }
+      { test: /\.png$/, loader: 'url-loader?limit=20480&name=images/[hash].[ext]' },
+      { test: /\.jpg|\.svg$/, loader: 'file-loader?name=images/[hash].[ext]' },
+      { test: /\.tsx?$/, loaders: ['react-hot-loader/webpack', 'ts-loader'] }
     ]
   },
   resolve: {
     extensions: ['.webpack.js', '.tsx', '.ts', '.js'],
     modules: [
-      path.join(__dirname, "web/static/app"),
-      "node_modules"
+      path.join(__dirname, 'web/static/app'),
+      'node_modules'
     ]
   },
   plugins: [
-    new webpack.optimize.CommonsChunkPlugin({ name: "vendor", filename: "[name].js" }),
+    new webpack.optimize.CommonsChunkPlugin({ name: 'vendor', filename: '[name].js' }),
     new webpack.HotModuleReplacementPlugin()
   ]
 }
