ca9ce203b3b4dcffbba0927731b1583ca71b25d3
bingjie.huang <bingjie.huang@shuyun.com>
2017-03-16 12:05:24 +0800

    use agent instead of ets

--- lib/reddit.ex
+++ lib/reddit.ex
@@ -1,43 +1,24 @@
 defmodule Coral.Reddit do
-  # use GenServer
   use HTTPoison.Base
 
   @endpoint "https://oauth.reddit.com"
-  @username Base.decode64("bGltUQ==") |> elem(1)
-  @password Base.decode64("aGJqMTQ0MzUxMg==") |> elem(1)
-  @client_id Base.decode64("QlpOQWtvWGpLcTB0ZHc=") |> elem(1)
-  @secret Base.decode64("SU1wRFQ4TXp4bGFUODFXSWVhLTlFclRLc3ZF") |> elem(1)
 
-  def start_link do
-    table = :ets.new(:reddit, [:named_table, :set, :protected])
-    body = HTTPoison.post("https://www.reddit.com/api/v1/access_token", {:form, [{:grant_type, "password"}, {:username, "limQ"}, {:password, "hbj1443512"}]}, [{"Authorization", "Basic " <> Base.encode64("BZNAkoXjKq0tdw:IMpDT8MzxlaT81WIea-9ErTKsvE")}])
-          |> elem(1)
-          |> Map.get(:body)
-          |> Poison.decode
-          |> elem(1)
-    
-    :ets.insert(table, {"token", body})
-    # GenServer.start_link(__MODULE__, :ok, [])
+  def start_link() do
+    Agent.start_link fn -> init() end, name: :reddit
   end
 
-  def process_url(url) do
-    @endpoint <> url
-  end
+  def process_url(url), do: @endpoint <> url
 
   def process_request_headers(term) do
-    %{"token_type" => token_type, "access_token" => access_token} = :ets.lookup(:reddit, "token") |> List.first |> elem(1)
+    %{"token_type" => token_type, "access_token" => access_token} = Agent.get(:reddit, fn body -> body end)
     term ++ [{"Authorization", token_type <> " " <> access_token}]
   end
 
-  # def init(_) do
-  #   table = :ets.new(:reddit_token, [:named_table, :set, :protected])
-  #   body = HTTPoison.post("https://www.reddit.com/api/v1/access_token", {:form, [{:grant_type, "password"}, {:username, "limQ"}, {:password, "hbj1443512"}]}, [{"Authorization", "Basic " <> Base.encode64("BZNAkoXjKq0tdw:IMpDT8MzxlaT81WIea-9ErTKsvE")}])
-  #         |> elem(1)
-  #         |> Map.get(:body)
-  #         |> Poison.decode
-  #         |> elem(1)
-
-  #   :ets.insert(table, body）
-  #   {:ok, body}
-  # end
+  def init do
+    HTTPoison.post("https://www.reddit.com/api/v1/access_token", {:form, [{:grant_type, "password"}, {:username, "limQ"}, {:password, "hbj1443512"}]}, [{"Authorization", "Basic " <> Base.encode64("BZNAkoXjKq0tdw:IMpDT8MzxlaT81WIea-9ErTKsvE")}])
+    |> elem(1)
+    |> Map.get(:body)
+    |> Poison.decode
+    |> elem(1)
+  end
 end
