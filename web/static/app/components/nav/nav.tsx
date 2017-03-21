70fa120fab38437b7ed4f6c48dbde004bb93fbfb
bingjie.huang <bingjie.huang@shuyun.com>
2017-03-21 20:01:37 +0800

    nav menu now dynamic match

--- web/static/app/components/nav/nav.tsx
+++ web/static/app/components/nav/nav.tsx
@@ -1,33 +1,20 @@
 import * as React from 'react'
 import { Nav as Navv, INavLink } from 'office-ui-fabric-react'
 import { Link } from 'react-router-dom'
+import { withRouter, IWithRouter } from 'react-router'
 
 import { groups } from '../../config/nav_config'
 import * as Style from './nav_style'
 
-export default class Nav extends React.Component<void, void> {
-	private static contextTypes = {
-		router: React.PropTypes.shape({
-			history: React.PropTypes.shape({
-				createHref: React.PropTypes.func.isRequired,
-				push: React.PropTypes.func.isRequired,
-				replace: React.PropTypes.func.isRequired
-			}).isRequired
-		}).isRequired
-	}
-	public context: {
-		router: {
-			history: {
-				push: (url: string) => void
-			}
-		}
-	}
+@withRouter
+export default class Nav extends React.Component<Partial<IWithRouter>, void> {
 	public render() {
+		const pathname = this.props.location!.pathname.substr(1)
 		return (
 			<Navv
 				className={Style.NAV}
 				groups={groups}
-				selectedKey="coral"
+				selectedKey={pathname}
 				onLinkClick={this.linkClick}
 			/>
 		)
@@ -35,6 +22,6 @@ export default class Nav extends React.Component<void, void> {
 
 	private linkClick = (event: React.MouseEvent<HTMLDivElement>, nav: INavLink) => {
 		event.preventDefault()
-		this.context.router.history.push(nav.url)
+		this.props.history!.push(nav.url)
 	}
 }
