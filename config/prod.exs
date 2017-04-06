0956f527a0a45467f1cac9812c76d7b306707c38
bingjie.huang <bingjie.huang@shuyun.com>
2017-04-07 00:17:04 +0800

    prod url filter

--- config/prod.exs
+++ config/prod.exs
@@ -13,7 +13,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :Coral, Coral.Endpoint,
   http: [port: 8888],
-  # url: [host: "example.com", port: 80],
+  url: [host: "corol.me", port: 80],
   cache_static_manifest: "priv/static/manifest.json"
   # secret_key_base: System.get_env("SECRET_KEY_BASE")
 
