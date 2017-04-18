8dcf4888b2aac334be5aced58c8316a8f5eb7475
bingjie.huang <bingjie.huang@shuyun.com>
2017-04-18 19:52:12 +0800

    new function -- guest now can push message to slack

--- web/static/app/views/main/main.tsx
+++ web/static/app/views/main/main.tsx
@@ -6,7 +6,9 @@ import * as Style from "./main_style"
 export default class Main extends React.Component<void, void> {
 	public render() {
 		return (
-			<div className={Style.MAIN}>おかえりなさい<Emoji.Interesting /></div>
+			<div className={Style.MAIN}>おかえりなさい<Emoji.Interesting />
+				Chrome有bug，socket连不上，slack实时消息和聊天室无法使用。除非你换火狐😂
+			</div>
 		)
 	}
 }
--- web/static/app/views/slack/slack.tsx
+++ web/static/app/views/slack/slack.tsx
@@ -2,11 +2,12 @@ import * as React from "react"
 import Store, { lift, inject } from "meng"
 import Messages from "./messages/messages"
 import Title from "./title/title"
+import TextInput from "./textinput/textinput"
 import Users from "./users/users"
 import Flex from "../../components/flex/flex"
 import * as Style from "./slack_style"
 
-import { list, user, connect, push } from "../../apis/slack_api"
+import { list, user, connect } from "../../apis/slack_api"
 import { ISlackListType, ISlackUserType, ISlackMessage } from "../../types/slack_type"
 
 type Props = {
@@ -33,6 +34,7 @@ export default class Slack extends React.Component<Props, void> {
 		return (
 			<div className={Style.SLACK}>
 				<Title />
+				<TextInput />
 				<Flex flexGrow={1} flexDirection={"row"}>
 					<Messages newmsg={this.props.newmsg} post={this.props.post} user={this.props.user} latest={this.props.latest} />
 					<Users user={this.props.user} />
