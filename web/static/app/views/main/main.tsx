eebf0c695feb80bd51783a7152fbbac11f26ba7c
bingjie.huang <bingjie.huang@shuyun.com>
2017-04-19 10:53:04 +0800

    main content

--- web/static/app/views/main/main.tsx
+++ web/static/app/views/main/main.tsx
@@ -7,7 +7,6 @@ export default class Main extends React.Component<void, void> {
 	public render() {
 		return (
 			<div className={Style.MAIN}>おかえりなさい<Emoji.Interesting />
-				Chrome有bug，socket连不上，slack实时消息和聊天室无法使用。除非你换火狐😂
 			</div>
 		)
 	}
