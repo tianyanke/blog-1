5caeaa848d3347c5571c1f5d9670c5e161b35d3a
bingjie.huang <bingjie.huang@shuyun.com>
2017-05-17 15:44:55 +0800

    we are reactive

--- web/static/app/views/main/main.tsx
+++ web/static/app/views/main/main.tsx
@@ -7,6 +7,11 @@ export default class Main extends React.Component<void, void> {
 	public render() {
 		return (
 			<div className={Style.MAIN}>おかえりなさい<Emoji.Interesting />
+				<a href="http://www.reactivemanifesto.org/">
+					<img
+						style={{ border: 0, position: "fixed", right: 0, top: 0, zIndex: 9000 }}
+						src="//d379ifj7s9wntv.cloudfront.net/reactivemanifesto/images/ribbons/we-are-reactive-white-right.png" />
+				</a>
 			</div>
 		)
 	}
